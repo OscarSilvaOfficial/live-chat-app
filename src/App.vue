f03e40526831f96bd190fb1ac25e9e7bbc90b845
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-04-25 19:44:46 -0300

    Add app web

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,92 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div v-for="data in globalMessages" :key="data.id">
+      <Message :name="data.name" :message="data.message" />
+    </div>
+    <form>
+      <v-text-field
+        class="inputText"
+        v-model="userName"
+        label="Nome"
+        solo
+      ></v-text-field>
+      <v-text-field
+        class="inputText"
+        v-model="userMessage"
+        label="Mensagem"
+        solo
+      ></v-text-field>
+      <v-btn @click="submitForm()">Enviar</v-btn>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { io } from "socket.io-client";
+import Message from "./components/Message";
+
+const socketIo = io("http://localhost:5000");
 
 export default {
   name: "App",
+
   components: {
-    HelloWorld,
+    Message,
+  },
+
+  mounted() {
+    this.renderMessage();
+  },
+
+  data() {
+    return {
+      userName: "",
+      userMessage: "",
+      globalMessages: [],
+    };
+  },
+
+  methods: {
+    renderMessage() {
+      socketIo.on("receivedMessage", (messages) => {
+        this.globalMessages.push(messages);
+      });
+    },
+
+    submitForm() {
+      const messageData = {
+        name: this.userName,
+        message: this.userMessage,
+      };
+
+      socketIo.emit("sendMessage", messageData);
+
+      this.globalMessages.push(messageData);
+    },
   },
 };
 </script>
+<style lang="scss" scope>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 
-<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: Montserrat;
+  margin-top: 1rem;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    max-width: 800px;
+    width: 100%;
+
+    button {
+      margin: 8px 0 0;
+      background: rgb(92 187 246);
+      font-weight: 600;
+    }
+  }
 }
 </style>
---
+++ src/components/Message.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card class="mx-auto" color="#26c6da" dark max-width="400" id="message-box">
+    <v-card-text class="headline font-weight-bold">
+      "{{ message }}"
+    </v-card-text>
+
+    <v-card-actions>
+      <v-list-item class="grow">
+        <v-list-item-avatar color="grey darken-3">
+          <v-img
+            class="elevation-6"
+            alt=""
+            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+          ></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ name }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  props: ["name", "message"],
+};
+</script>
+<style lang="scss" scope>
+#message-box {
+  margin-bottom: 1rem;
+  width: 800px;
+  max-width: 800px !important;
+}
+</style>
